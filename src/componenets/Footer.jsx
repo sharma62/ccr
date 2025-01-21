a332d169837c1b7e349331650cdc5cf0ebf3bd67
SURAJ KUMAR SHARMA <75290099+sharma62@users.noreply.github.com>
2025-01-21 23:53:52 +0530

    Update Footer.jsx

--- src/componenets/Footer.jsx
+++ src/componenets/Footer.jsx
@@ -50,7 +50,7 @@ const Footer = (props) => {
           </div>
           <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
           <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
-            © 2024{" "}
+            © 2025{" "}
             <a href="#" className="hover:underline">
               {props.app_name}
             </a>
